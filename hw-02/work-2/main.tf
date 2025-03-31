d70e4948ebbf4443dcc979ace924031481a5db21
Kulikova-A18 <81427431+Kulikova-A18@users.noreply.github.com>
2025-03-31 12:41:49 +0300

    Create main.tf

---
+++ hw-02/work-2/main.tf
@@ -0,0 +1,47 @@
+resource "yandex_vpc_network" "develop" {
+  name = var.vpc_name
+}
+resource "yandex_vpc_subnet" "develop" {
+  name           = var.vpc_name
+  zone           = var.default_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.default_cidr
+}
+
+
+data "yandex_compute_image" "ubuntu" {
+  family = "ubuntu-2004-lts"
+}
+resource "yandex_compute_instance" "platform" {
+#  name        = "netology-develop-platform-web"
+#  platform_id = "standart-v1"
+  name =  var.vm_web_instance_name
+  platform_id = var.vm_web_platform_id
+
+  resources {
+#    cores         = 2
+#    memory        = 1
+#    core_fraction = 5
+    cores         = var.vm_web_resources.cores
+    memory        = var.vm_web_resources.memory
+    core_fraction = var.vm_web_resources.core_fraction
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
+
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  }
+
+}
