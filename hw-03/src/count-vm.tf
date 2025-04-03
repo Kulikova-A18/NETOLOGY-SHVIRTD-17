1f02b82f38276dbf9d4f998fecb4de7e6cb6ea95
Kulikova-A18 <81427431+Kulikova-A18@users.noreply.github.com>
2025-04-03 16:54:01 +0300

    Create count-vm.tf

---
+++ hw-03/src/count-vm.tf
@@ -0,0 +1,29 @@
+resource "yandex_compute_instance" "web" {
+  count = 2  # 2 экземпляра ВМ
+  name        = "web-${count.index + 1}"   # "web-1" и "web-2"
+  platform_id = "standard-v1"              # тип виртуальной машины
+  zone        = var.default_zone           # размещен экземпляр, задается из переменной
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8chrq89mmk8tqm85r8"
+      size     = 30                 # размер диска
+      type     = "network-hdd"      # тип диска
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+    security_group_ids = ["enppnvqd1388ibcdil9p"]
+  }
+
+  resources {
+    memory = 2    # объем оперативной памяти в ГБ
+    cores  = 2    # кол-во виртуальных процессоров
+  }
+
+  metadata = {
+    ssh-keys = file("~/yan.pub")
+  }
+}
