b27ae354cbf6bcaaa1880623fe95443fd4673847
Kulikova-A18 <81427431+Kulikova-A18@users.noreply.github.com>
2025-04-07 11:42:04 +0300

    Update disk_vm.tf

--- hw-03/src/disk_vm.tf
+++ hw-03/src/disk_vm.tf
@@ -1,73 +1,103 @@
-variable "default_zone" {
-  description = "Default availability zone"
-  type        = string
+
+variable "storage_secondary_disk" {
+  type = list(object({
+    for_storage = object({
+      type       = string   # Тип диска
+      size       = number   # Размер диска
+      count      = number   # Количество дисков
+      block_size = number   # Размер блока диска
+      name       = string   # Имя диска
+    })
+  }))
+
+  default = [
+    {
+      for_storage = {
+        type       = "network-hdd"    # тип диска
+        size       = 1                # размер диска
+        count      = 3                # 3 диска
+        block_size = 4096             # размер блока на 4096 байт
+        name       = "disk"           # Имя диска
+      }
+    }
+  ]
 }
 
-variable "disk_size" {
-  description = "Size of the secondary disks in GB"
-  type        = number
-  default     = 1
+resource "yandex_compute_disk" "disks" {
+  count       = var.storage_secondary_disk[0].for_storage.count                         # Количество дисков
+  name        = "${var.storage_secondary_disk[0].for_storage.name}-${count.index + 1}"  # Имя диска с индексом
+  type        = var.storage_secondary_disk[0].for_storage.type                          # Тип диска
+  size        = var.storage_secondary_disk[0].for_storage.size                          # Размер диска
+  block_size  = var.storage_secondary_disk[0].for_storage.block_size                    # Размер блока диска
 }
 
-variable "boot_disk_size" {
-  description = "Size of the boot disk in GB"
-  type        = number
-  default     = 20
+variable "yandex_compute_instance_storage" {
+  type = object({
+    storage_resources = object({
+      cores         = number    # Количество ядер
+      memory        = number    # Объем памяти
+      core_fraction  = number   # Доля ядра
+      name          = string    # Имя ВМ
+      zone          = string    # Зона ВМ
+    })
+  })
+
+  default = {
+    storage_resources = {
+      cores         = 2                # количество ядер
+      memory        = 2                # объем памяти
+      core_fraction  = 5               # доля ядра
+      name          = "storage"        # Имя ВМ
+      zone          = "ru-central1-a"  # Зона размещения
+    }
+  }
 }
 
-variable "instance_memory" {
-  description = "Memory for the instance in GB"
-  type        = number
-  default     = 2
-}
-
-variable "instance_cores" {
-  description = "Number of cores for the instance"
-  type        = number
-  default     = 2
-}
-
-resource "yandex_compute_disk" "storage_disk" {
-  for_each = toset(["disk1", "disk2", "disk3"])
-
-  name = "storage-disk-${each.key}"
-  size = var.disk_size
-  type = "network-hdd"
-  zone = var.default_zone
+variable "boot_disk_storage" {
+  type = object({
+    size = number   # Размер диска
+    type = string   # Тип диска
+  })
+  
+  default = {
+    size = 5                  # размер диска
+    type = "network-hdd"      # тип диска
+  }
 }
 
 resource "yandex_compute_instance" "storage" {
-  name        = "storage"
-  platform_id = "standard-v1"
-  zone        = var.default_zone
+  name = var.yandex_compute_instance_storage.storage_resources.name # Имя ВМ
+  zone = var.yandex_compute_instance_storage.storage_resources.zone # Зона размещения ВМ
+
+  resources {
+    cores         = var.yandex_compute_instance_storage.storage_resources.cores          # Количество ядер
+    memory        = var.yandex_compute_instance_storage.storage_resources.memory         # Объем памяти
+    core_fraction  = var.yandex_compute_instance_storage.storage_resources.core_fraction # Доля ядра
+  }
 
   boot_disk {
     initialize_params {
-      image_id = "fd8chrq89mmk8tqm85r8"
-      size     = var.boot_disk_size
-      type     = "network-hdd"
+      image_id = data.yandex_compute_image.ubuntu-2004-lts.image_id   # ID образа диска
+      type     = var.boot_disk_storage.type                           # Тип диска
+      size     = var.boot_disk_storage.size                           # Размер диска
     }
   }
 
-  network_interface {
-    subnet_id           = yandex_vpc_subnet.develop.id
-    nat                 = true
-    security_group_ids  = ["enpce7huph0t1kvtivnk"]
-  }
-
-  resources {
-    memory = var.instance_memory
-    cores  = var.instance_cores
-  }
-
   metadata = {
-    ssh-keys = file("~/.ssh/id_ed25519.pub")
+    ssh-keys           = file("~/.ssh/id_ed25519.pub")               
+    serial-port-enable = "1"
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.develop.id               # ID подсети ВМ
+    nat                = true                                       # NAT
+    security_group_ids = [yandex_vpc_security_group.example.id]     # группа безопасности ВМ
   }
 
   dynamic "secondary_disk" {
-    for_each = yandex_compute_disk.storage_disk
+    for_each = yandex_compute_disk.disks.*.id                       # все вторичные диски, созданные ранее
     content {
-      disk_id = secondary_disk.value.id
+      disk_id = secondary_disk.value                                # Привязываем каждый вторичный диск к ВМ
     }
   }
 }
