b3011dec894932d1d086e7cb6986c4db13532398
Kulikova-A18 <81427431+Kulikova-A18@users.noreply.github.com>
2025-09-18 10:02:57 +0300

    Add files via upload

---
+++ clopro-homeworks/terraform15.3/variables.tf
@@ -0,0 +1,17 @@
+]
+variable "yandex_cloud_id" {
+  default = "b1g*****7s"
+}
+
+variable "yandex_folder_id" {
+  default = "b1*****4"
+}
+
+variable "b-zone" {
+  default = "ru-central1-b"
+}
+
+variable "lamp-instance-image-id" {
+  default = "fd827b91d99psvq5fjit"
+}
+
